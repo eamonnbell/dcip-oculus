d7fd199d839c8cb2acd60daa507095268a518fb1
Eamonn Bell <pseudoboarder@gmail.com>
2016-03-07 07:55:25 -0500

    Add fake stuff.

--- generalviz/data/faker.py
+++ generalviz/data/faker.py
@@ -3,7 +3,7 @@ import random
 genres = ["pop", "rock", "classical", "jazz", "metal", "country", "hip-hop"]
 
 print "volume,genre"
-for i in range(100):
+for i in range(20):
     volume = random.randint(25, 100)
     genre = random.choice(genres)
     print "{},{}".format(volume, genre)
