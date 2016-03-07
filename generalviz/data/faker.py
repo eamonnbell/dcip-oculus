3c8a279ea1ace60d2c6a2a2e06d9492f05f8ef32
Eamonn Bell <pseudoboarder@gmail.com>
2016-03-07 07:52:23 -0500

    Add datafaker.

---
+++ generalviz/data/faker.py
@@ -0,0 +1,10 @@
+import random
+
+genres = ["pop", "rock", "classical", "jazz", "metal", "country", "hip-hop"]
+
+print "volume,genre"
+for i in range(100):
+    volume = random.randint(25, 100)
+    genre = random.choice(genres)
+    print "{},{}".format(volume, genre)
+
